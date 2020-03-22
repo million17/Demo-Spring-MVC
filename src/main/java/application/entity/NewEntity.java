7bf7efe4de78e2b99034279fd8579c40c84a2591
trieu.ngoquang <ngoquangtrieu99tb@gmail.com>
2020-03-22 11:36:35 +0700

    update jpa config

---
+++ src/main/java/application/entity/NewEntity.java
@@ -0,0 +1,65 @@
+package application.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "news")
+public class NewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "thumbnail")
+    private String thumbnail;
+
+    @Column(name = "short_desc", columnDefinition = "TEXT")
+    private String shortDesc;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
